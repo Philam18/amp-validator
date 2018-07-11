2303defb5acdaa74f91895e13d3c4c24f3041942
Philip Lam <philip@donkeymob.com>
2018-07-11 16:54:19 -0400

    Changed for piping/command line arguments

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-// const URL = "http://192.168.99.100:32771/sitemap.html";
-// const URL = "https://www.livingsharp.com/sitemap.html";
-
-require('./extract_amp_urls.js').extractAMPurls(
-	'https://www.fightful.com/sitemap.xml',
-	( filename )=>{
-
-	},
-	{"safe_mode" : false, "wait_time" : 0}
-);
-
-//
-// require('./validate_amp_urls').validateAMPurls(
-// 	'SITEMAP_URL_20180710T142839.txt',
-// 	( report_filename )=>{
-// 		console.log( report_filename)
-// 	},
-// 	{"safe" : true, "wait_time" : 1000}
-// );
+var str = 'https://www.fightful.com/wrestling/chris-jericho-says-sin-cara-let-him-borrow-mask-raw-angle?amp';
+console.log(str.length);
